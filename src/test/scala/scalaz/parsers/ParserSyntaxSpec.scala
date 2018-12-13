94977bdff5e2e3d4824af6ece40d2ad18d65873a
sergei-shabanau <serge.shabanau@gmail.com>
2018-12-13 10:47:22 -0500

    #0 - Added more tests

--- src/test/scala/scalaz/parsers/ParserSyntaxSpec.scala
+++ src/test/scala/scalaz/parsers/ParserSyntaxSpec.scala
@@ -1,13 +1,13 @@
 package scalaz.parsers
 
 import org.specs2.mutable.Specification
-import scalaz.tc.{ Applicative, ApplicativeClass, instanceOf }
+import scalaz.tc._
 
 class ParserSyntaxSpec extends Specification {
 
-  type Parser[A] = String => (String, List[A])
+  private type Parser[A] = String => (String, List[A])
 
-  object Parser extends ParserSyntax[Parser] with ParserIsoSyntax[Parser, Option, Option] {
+  private object Parser extends ParserSyntax[Parser] with ParserIsoSyntax[Parser, Option, Option] {
     override val iso: IsoClass[Option, Option] =
       new IsoClass[Option, Option] {}
     override def delay[A](pa: => Parser[A]): Parser[A] =
@@ -16,10 +16,15 @@ class ParserSyntaxSpec extends Specification {
       s => pa(s) match { case (s1, aa) => s1 -> aa.map(iso.to).collect { case Some(b) => b } }
   }
 
-  object PIso extends IsoClass[Option, Option]
-
-  object Instances {
+  private object Instances {
     import scalaz.Scalaz.monadApplicative
+
+    implicit val functorParser: Functor[Parser] = instanceOf(
+      new FunctorClass[Parser] {
+        override def map[A, B](fa: Parser[A])(f: A => B): Parser[B] =
+          s => fa(s) match { case (s1, aa) => s1 -> aa.map(f) }
+      }
+    )
 
     implicit val applicativeParser: Applicative[Parser] = instanceOf(
       new ApplicativeClass[Parser] {
@@ -31,7 +36,7 @@ class ParserSyntaxSpec extends Specification {
           s2 -> ff.flatMap(aa.map(_))
         }
         override def map[A, B](fa: Parser[A])(f: A => B): Parser[B] =
-          ap(fa)(pure(f))
+          functorParser.map(fa)(f)
       }
     )
 
@@ -40,7 +45,8 @@ class ParserSyntaxSpec extends Specification {
         override def or[A](f1: Parser[A], f2: => Parser[A]): Parser[A] = { s =>
           val (s1, r1) = f1(s)
           val (s2, r2) = f2(s)
-          if (s1.length <= s2.length) s1 -> r1 else s2 -> r2
+          val diff     = s1.lengthCompare(s2.length)
+          if (diff < 0) s1 -> r1 else if (diff > 0) s2 -> r2 else s1 -> (r1 ::: r2)
         }
       }
     )
@@ -68,23 +74,47 @@ class ParserSyntaxSpec extends Specification {
     }
   }
 
-  private val parseChar: Parser[Char] =
-    s => s.headOption.fold("" -> List.empty[Char])(c => s.drop(1) -> List(c))
+  // parser that consumes one char from the input
+  private val char: Parser[Char] =
+    s => s.headOption.map(c => s.drop(1) -> List(c)).getOrElse("" -> Nil)
 
+  // iso that formats the char `x` as `char(x)`
   private val describe: Iso[Option, Option, Char, String] =
-    iso.lift[Char, String]("char(" + _ + ")", _.drop(5).headOption.getOrElse(0))
+    iso.lift("char(" + _ + ")", _.drop(5).headOption.getOrElse(0))
 
   "isoMap" should {
     "map over parsed value with an iso" in {
-      isoMap(parseChar)(describe)("a") must_=== (("", List("char(a)")))
-      (parseChar ∘ describe)("a") must_=== (("", List("char(a)")))
-      (parseChar ∘ describe)("ab") must_=== (("b", List("char(a)")))
+      isoMap(char)(describe)("a") must_=== ("" -> List("char(a)"))
+      (char ∘ describe)("a") must_=== (""      -> List("char(a)"))
+      (char ∘ describe)("ab") must_=== ("b"    -> List("char(a)"))
     }
   }
 
   "many" should {
-    "yield a list of things from one thing" in {
-      parseChar.many.apply("ab") must_=== (("", List(List('a', 'b'))))
+    "yield a list of results from input" in {
+      char.many.apply("ab") must_=== (""              -> List(List('a', 'b')))
+      (char ∘ describe).many.apply("ab") must_=== ("" -> List(List("char(a)", "char(b)")))
+    }
+  }
+
+  "combinators" >> {
+    "and (product)" in {
+      (char /\ char).apply("a") must_=== (""  -> List())
+      (char /\ char).apply("ab") must_=== ("" -> List('a' -> 'b'))
+    }
+    "or (coproduct)" in {
+      (char \/ char).apply("a") must_=== (""       -> List(Left('a'), Right('a')))
+      (char \/ char).apply("ab") must_=== ("b"     -> List(Left('a'), Right('a')))
+      (char.many \/ char).apply("ab") must_=== ("" -> List(Left(List('a', 'b'))))
+      (char \/ char.many).apply("ab") must_=== ("" -> List(Right(List('a', 'b'))))
+    }
+    "or (alternative)" in {
+      (char || char).apply("") must_=== (""  -> Nil)
+      (char || char).apply("a") must_=== ("" -> List('a', 'a'))
+
+      def eq(c: Char): Char => Option[Char]            = Some(_).filter(_ == c)
+      def is(c: Char): Iso[Option, Option, Char, Char] = iso.liftF(eq(c), eq(c))
+      ((char ∘ is('a')) || (char ∘ is('b'))).apply("a") must_=== ("" -> List('a'))
     }
   }
 }
