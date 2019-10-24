37d511597a950be97fae89ac112a8e79e73f49e2
sergei-shabanau <serge.shabanau@gmail.com>
2019-10-24 12:30:06 -0400

    #0 - Dropped v0.1.0 code

--- src/main/scala/org/spartanz/parserz/shaper/types.scala
+++
@@ -1,6 +0,0 @@
-package org.spartanz.parserz.shaper
-
-object types {
-  //type s[S, F[_, _], E, G[_], T] = S => ??? // TODO: figure this out (go from source to abstract syntax tree) .. to Cofree/Free tree representation
-
-}
--- src/main/scala/org/spartanz/parserz/syntax/category.scala
+++
@@ -1,5 +0,0 @@
-package org.spartanz.parserz.syntax
-
-import org.spartanz.parserz.tc.Category.CategorySyntax
-
-object category extends CategorySyntax
